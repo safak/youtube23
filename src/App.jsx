fea1638121aef0a6de1350b77776562a5bd5cfcb
‘Diontre <diontre01@gmail.com>
2023-04-23 20:08:02 -0500

    3d

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
+import styled from 'styled-components';
+import Contact from "./Components/Contact"
+import Hero from "./Components/Hero"
+import Who from "./Components/Who"
+import Works from "./Components/Works"
+
+
+const Container = styled.div`
+  height: 100vh;
+  background-color: red;
+
+`
+const Button = styled.button`
+  width: 200px;
+  height: 50px;
+  background-color: red;
+
+`
+
+
 function App() {
 
   return (
-    <div>
-      Hello world!
-    </div>
+    <Container>
+      <Hero/>
+      <Works/>
+      <Who/>
+      <Contact/>
+      <Button/>
+    </Container>
   )
 }
+
+
 
 export default App
